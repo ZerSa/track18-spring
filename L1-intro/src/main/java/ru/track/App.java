f3769619e47c3796ec506eb46c4f1c3b2fb9ef1c
ZerSa <sasha-zendrikova@yandex.ru>
2018-02-28 01:30:44 +0300

    added intro task

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -12,20 +12,25 @@ package ru.track;
 public class App {
 
     public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
-    public static final String FIELD_NAME = "name";
-    public static final String FIELD_GITHUB = "github";
-    public static final String FIELD_EMAIL = "email";
+    public static final String FIELD_NAME = "Zendrikova Sasha";
+    public static final String FIELD_GITHUB = "https://github.com/ZerSa/track18-spring";
+    public static final String FIELD_EMAIL = "sasha-zendrikova@yandex.ru";
 
     public static void main(String[] args) throws Exception {
-       // 1) Use Unirest.post()
+        HttpResponse<JsonNode> r = Unirest.post(URL)
+                .header("accept", "application/json")
+                .field("name", FIELD_NAME)
+                .field("github", FIELD_GITHUB)
+                .field("email", FIELD_EMAIL)
+                .asJson();
+
+        boolean success = r.getBody().getObject().get("success");
+
+       // 1) Use Unirest.post(URL)
         // 2) Get response .asJson()
         // 3) Get json body and JsonObject
         // 4) Get field "success" from JsonObject
-
-
-
-
-        boolean success = false;
+        //boolean success = false;
     }
 
 }
