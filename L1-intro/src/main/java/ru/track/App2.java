0108e568aa203f18d8bbec20a4111c604ff7513b
ZerSa <sasha-zendrikova@yandex.ru>
2018-02-28 02:03:15 +0300

    copied intro_task into App2.javaas required

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -1,12 +1,27 @@
 package ru.track;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class App2 {
 
-    public static void main(String[] args) {
-        String str = StringUtils.capitalize(args[0]);
-        System.out.println(str);
+    public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
+    public static final String FIELD_NAME = "Zendrikova Sasha";
+    public static final String FIELD_GITHUB = "https://github.com/ZerSa/track18-spring";
+    public static final String FIELD_EMAIL = "sasha-zendrikova@yandex.ru";
+
+    public static void main(String[] args) throws Exception {
+        HttpResponse<JsonNode> r = Unirest.post(URL)
+                .header("accept", "application/json")
+                .field("name", FIELD_NAME)
+                .field("github", FIELD_GITHUB)
+                .field("email", FIELD_EMAIL)
+                .asJson();
+
+        System.out.println(r.getBody().getObject().get("success"));
+
     }
 }
